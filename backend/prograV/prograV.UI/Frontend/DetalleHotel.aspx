6c09c28297831a3ab8f2095576656529dd0597cf
Diego Castro <diegocd0506@gmail.com>
2016-08-20 10:43:44 -0600

    detalle hotel

--- backend/prograV/prograV.UI/Frontend/DetalleHotel.aspx
+++ backend/prograV/prograV.UI/Frontend/DetalleHotel.aspx
@@ -1,3 +1,6 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/FrontendView.Master" AutoEventWireup="true" CodeBehind="DetalleHotel.aspx.cs" Inherits="prograV.UI.Frontend.DetalleHotel" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+<asp:ListView runat="server" ID="lvDetalleHotel">
+
+</asp:ListView>
 </asp:Content>
--- backend/prograV/prograV.UI/Frontend/DetalleHotel.aspx.cs
+++ backend/prograV/prograV.UI/Frontend/DetalleHotel.aspx.cs
@@ -9,9 +9,26 @@ namespace prograV.UI.Frontend
 {
     public partial class DetalleHotel : System.Web.UI.Page
     {
+        private BL.IHotel hotelBl = new BL.Hotel();
+        private string update;
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!Page.IsPostBack)
+            {
+                update = Request.QueryString["idHotel"];
+                if (!String.IsNullOrEmpty(update))
+                {
+                var detalleHotel = hotelBl.BuscarHotelporId(int.Parse(update));
 
+                };
+            }
         }
+    }
+    class hotelDetalle
+    {
+        public string NombreHotel { get; set; }
+        public string Pais { get; set; }
+        public string DescripcionHotel { get; set; }
+        public int idHotel { get; set; }
     }
 }
